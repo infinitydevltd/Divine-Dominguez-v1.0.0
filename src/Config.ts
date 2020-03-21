0c17a4fdf77453da753cae295d15cdc4540e573d
infinitycoding222 <58121837+infinitycoding222@users.noreply.github.com>
2020-03-22 01:34:12 +0200

    Update Config.ts

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
-export let token: string = "NjYxNzM1MTM1Njk1MDc3Mzg2.Xkrk8g.oSoDYZK3FyqnIIjuM4PJ_Sghmdw"; // This is our bot token
-export let ownersid: string[] = ["473276250815856650", "540219416726601739"]; // this is our array of bot owners
-export let defprefix: string = "c:?"; // this is our bot prefix
-export let databaseName: string = "divindedb"
+export let token: string = "smh"; // This is our bot token
+export let ownersid: string[] = ["id2", "id"]; // this is our array of bot owners
+export let defprefix: string = "prefix"; // this is our bot prefix
+export let databaseName: string = "db"
